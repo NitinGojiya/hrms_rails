4a65700cd47f4288a9fdaa8dd7319b981df5703e
nitin <nitingojiya2000@gmail.com>
2025-06-19 12:24:17 +0530

    employee profile check(employee managment)

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,6 +3,9 @@ class AdminsController < ApplicationController
 
   def index
     @user = Current.session.user
+    @leaves_pending = Leafe.pending.order(updated_at: :desc)
+    @leaves_accept = Leafe.accept.order(updated_at: :desc)
+    @leaves = Leafe.all.order(updated_at: :desc)
   end
   def leave
     @user = Current.session.user
@@ -12,6 +15,15 @@ class AdminsController < ApplicationController
     @leaves = Leafe.all.order(updated_at: :desc)
   end
 
-  def leavea
+  def employee
+    @user = Current.session.user
+    @leaves_pending = Leafe.pending
+    @employees = User.all.order(created_at: :asc)
+  end
+
+  def viewemployee
+    @user = Current.session.user
+    @leaves_pending = Leafe.pending
+    @profile = Profile.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
   resource :admins
   get "admins/dashboard", to: "admins#index"
   get "admins/leave", to: "admins#leave"
+  get "admins/leave", to: "admins#leave"
+  get "admins/employee", to: "admins#employee"
   post "leave_aproved", to: "leaves#accept", as: "leave_aproved"
+  get "employee/profile/:id", to: "admins#viewemployee"
   # user routes
   resource :profiles
   get "profiles/:id", to: "profiles#show", as: :profile
