dc3bc1648b472f9939ae0cebfe4dbd43f306921d
nitin <nitingojiya2000@gmail.com>
2025-07-02 17:05:41 +0530

    seed file and screen project

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+user1 = User.create(email_address: 'sanjay123@gmail.com', password: "n12345678")
+user2 = User.create(email_address: 'nitin@gmail.com', password: "n12345678")
+admin = User.create(email_address: 'admin@gmail.com', password: "n12345678", role: 1)
+
+user2.build_profile(employee_name: 'nitin gojiya', department: 'Devloper', job_title: 'ror backend', phone_number: '9313295400', city: 'Rajkot', father_name: 'm', mother_name: 'k', acount_number: '12345678901234')
+tech = [ "ROR", "MERN", "Java" ]
+[ "hrms", "e-commerce", "Finacial" ].each_with_index do |project, index|
+  Project.create(p_name: project, start_date: Time.now+index.day, end_date: Time.now+(index*10).day, tech: tech[index])
+end
